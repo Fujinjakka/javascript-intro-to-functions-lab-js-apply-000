3ec014c8d64957c011c6dceafa59e2f259571c8e
Marco Calvieri <calvieri.marco@gmail.com>
2019-05-22 18:58:19 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+'Hello!'.toUpperCase()
+
+
+function whisper(string) {
+  return string.toLowerCase() 
+}
+'Hello!'.toLowerCase()
+
+
+function logShout(string) {
+  console.log("HELLO");
+  
+}
+"Hello!".toUpperCase()
+
+
+function logWhisper(string) {
+  console.log("hello");
+  
+}
+"HELLO!".toLowerCase()
+
+
+function sayHiToGrandma(string){
+if (string.toLowerCase() === string)
+  return "I can't hear you!"
+}
+
+
+
