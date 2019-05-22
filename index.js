01b321fa384a7bf5f95e3fa0faba5493dd9976a3
Marco Calvieri <calvieri.marco@gmail.com>
2019-05-22 19:00:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,8 +29,11 @@ if (string.toLowerCase() === string)
   return "I can't hear you!"
 }
 function sayHiToGrandma(string){
-if (string.toLowerCase() === string)
-  return "I can't hear you!"
+if (string.toUpperCase() === string)
+  return "YES INDEED!"
 }
-
+function sayHiToGrandma(string){
+if ("I love you, Grandma." === string)
+  return "I love you, too."
+}
 
